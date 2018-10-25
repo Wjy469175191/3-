3365c610c5d51493e080cc95ecd5ec1db5841ad6
Wjy469175191 <469175191@qq.com>
2018-10-25 20:16:53 +0800

    iconfont

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -6,18 +6,23 @@ export default class Nav extends React.Component {
     render(){
         return <div className="nav_box">
             <NavLink to="/wyyx" exact={true}>
+                <i className="iconfont icon-home"></i>
                 <span>首页</span>
             </NavLink>
             <NavLink to="/wyyx/shiwu" exact={true}>
+                <i className="iconfont icon-yk_fangkuai"></i>
                 <span>识物</span>
             </NavLink>
             <NavLink to="/wyyx/classify" exact={true}>
+                <i className="iconfont icon-chouti"></i>
                 <span>分类</span>
             </NavLink>
             <NavLink to="/wyyx/shoppingCart" exact={true}>
+                <i className="iconfont icon-icon--"></i>
                 <span>购物车</span>
             </NavLink>
             <NavLink to="/wyyx/proFile" exact={true}>
+                <i className="iconfont icon-geren10"></i>
                 <span>个人</span>
             </NavLink>
         </div>
