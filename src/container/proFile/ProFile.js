f97aa070050d7241ef762fad7d5cc03c8500cc64
Wjy469175191 <469175191@qq.com>
2018-10-31 17:33:19 +0800

    加上login

--- src/container/proFile/ProFile.js
+++ src/container/proFile/ProFile.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './index.less'
+import Login from "../../component/Login/Login"
 
 export default class Profile extends React.Component {
     render() {
-            return <div className='profile'>
-                 123
-            </div>
-        }
+        return <div className='profile'>
+            {/* <Login/> */}
+            123
+        </div>
+    }
 }
 
